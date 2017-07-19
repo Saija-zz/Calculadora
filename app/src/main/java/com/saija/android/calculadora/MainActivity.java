2093548fab21db069ceab33b233496df8ef2f43e
borrerju <julborre@gmail.com>
2017-07-19 16:47:35 -0500

    anadi otro layout mas power para la calculadora

--- app/src/main/java/com/saija/android/calculadora/MainActivity.java
+++ app/src/main/java/com/saija/android/calculadora/MainActivity.java
@@ -2,12 +2,12 @@ package com.saija.android.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText pantalla;
 
     private long operandoA = 0;
     private long operandoB = 0;
@@ -16,48 +16,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.basico);
-
-        /*
-        try {
-            pantalla = (EditText) findViewById(R.id.screen);
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        */
+        setContentView(R.layout.calculator_table);
     }
 
-    /*public void onClickNumbers(View view) {
-
+    public void onClickNumbers(View view) {
+        EditText pantalla = (EditText) findViewById(R.id.screen);
         Button leBoton = (Button)view;
-
         String txtBoton = leBoton.getText().toString();
+
         try{
             int numBoton = Integer.parseInt(txtBoton);
             StringBuilder strBuilder = new StringBuilder();
             strBuilder.append(pantalla.getText().toString());
             strBuilder.append(numBoton);
 
+            pantalla.setText("");
             pantalla.setText(strBuilder.toString());
 
         }catch(NumberFormatException nfe){
             pantalla.setText(nfe.getMessage());
         }
 
-    }*/
+    }
 
     public void onClickPi(View view) {
+        EditText pantalla = (EditText) findViewById(R.id.screen);
+        Log.i(this.getClass().getName(),"pantalla = " + pantalla);
+
+        pantalla.setText("");
         pantalla.setText(String.valueOf(Math.PI));
     }
 
     public void onClickOperator(View view) {
-        Button leBoton = (Button)view;
-
+        EditText pantalla = (EditText) findViewById(R.id.screen);
+        Button leBoton = (Button) view;
         String txtBoton = leBoton.getText().toString();
 
-        try{
+        Log.i("onClickOperator = ", "leBoton =  " + leBoton.toString());
 
-        }catch(Exception nfe){
+        try {
+
+        } catch (Exception nfe) {
             pantalla.setText(nfe.getMessage());
         }
 
